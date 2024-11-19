5d810d941ed3bec11f5efe9be0a2d7ecf0efd96e
Irthn1311 <nhuutri1311@gmail.com>
2024-11-19 14:14:05 +0700

    ...

---
+++ src/Interface/QuanLy.java
@@ -0,0 +1,12 @@
+package Interface;
+
+public interface QuanLy {
+    void Xem();
+    void Them();
+    void Sua();
+    void Xoa();
+    void TimKiem();
+    void ThongKe();
+    void DocFile();
+    void Ghi();
+}
